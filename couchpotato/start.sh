b5c77499e60b05adcafa38b85c6b5fa75c6b2ee1
Paul Kremer <pkremer@spurious.biz>
2018-01-08 00:49:33 +0100

    typo

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -3,7 +3,7 @@ echo "$(date): STARTING COUCHPOTATO"
 
 . /etc/couchpotato/userSetup.sh
 
-if -f /config/settings.ini; # optional, defaults to /data/.couchpotato/settings.ini
+if test -f /config/settings.ini; # optional, defaults to /data/.couchpotato/settings.ini
 then
 	exec sudo -Hu ${RUN_AS} python /CouchPotatoServer/CouchPotato.py --data_dir=/data --config_file=/config/settings.ini --console_log
 else
