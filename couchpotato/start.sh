6a198625bf64bfec7d6dac87e5ebb5b37cafdc6f
Paul Kremer <pkremer@spurious.biz>
2018-01-08 00:42:35 +0100

    Make separate volume for config options

--- couchpotato/start.sh
+++ couchpotato/start.sh
@@ -3,4 +3,9 @@ echo "$(date): STARTING COUCHPOTATO"
 
 . /etc/couchpotato/userSetup.sh
 
-exec sudo -Hu ${RUN_AS} python /CouchPotatoServer/CouchPotato.py --data_dir=/data --config_file=/config/settings.ini --console_log
+if -f /config/settings.ini; # optional, defaults to /data/.couchpotato/settings.ini
+then
+	exec sudo -Hu ${RUN_AS} python /CouchPotatoServer/CouchPotato.py --data_dir=/data --config_file=/config/settings.ini --console_log
+else
+	exec sudo -Hu ${RUN_AS} python /CouchPotatoServer/CouchPotato.py --data_dir=/data --console_log
+fi
